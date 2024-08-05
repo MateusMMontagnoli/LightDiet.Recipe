a90c9ed07a7ebc1b460660c56403fb7bfe6bb0b2
Mateus Marques Montagnoli <45606944+MateusMMontagnoli@users.noreply.github.com>
2024-08-04 23:06:50 -0300

    terminating category use cases

---
+++ tests/LightDiet.Recipe.UnitTests/Application/Category/ListCategories/ListCategoriesTestDataGenerator.cs
@@ -0,0 +1,41 @@
+﻿using LightDiet.Recipe.Application.UseCases.Category.ListCategories.Dto;
+
+namespace LightDiet.Recipe.UnitTests.Application.Category.ListCategories;
+
+public class ListCategoriesTestDataGenerator
+{
+    public static IEnumerable<object[]> GetInputsWithDefaultParameters(int times = 18)
+    {
+        var fixture = new ListCategoriesTestFixture();
+
+        var inputExample = fixture.GetExampleInput();
+
+        for (int i = 0; i < times; i++)
+        {
+            switch (i % 6)
+            {
+                case 0:
+                    yield return new object[] { new ListCategoriesInput() };
+                    break;
+                case 1:
+                    yield return new object[] { new ListCategoriesInput(inputExample.Page) };
+                    break;
+                case 2:
+                    yield return new object[] { new ListCategoriesInput(inputExample.Page, inputExample.PerPage) };
+                    break;
+                case 3:
+                    yield return new object[] { new ListCategoriesInput(inputExample.Page, inputExample.PerPage, inputExample.Search) };
+                    break;
+                case 4:
+                    yield return new object[] { new ListCategoriesInput(inputExample.Page, inputExample.PerPage, inputExample.Search, inputExample.Sort) };
+                    break;
+                case 5:
+                    yield return new object[] { inputExample };
+                    break;
+                default:
+                    yield return new object[] { new ListCategoriesInput() };
+                    break;
+            }
+        }
+    }
+}
---
+++ tests/LightDiet.Recipe.UnitTests/Application/Category/ListCategories/ListCategoriesTestFixture.cs
@@ -0,0 +1,42 @@
+﻿using LightDiet.Recipe.Application.UseCases.Category.ListCategories.Dto;
+using LightDiet.Recipe.Domain.SeedWork.SearchableRepository;
+using LightDiet.Recipe.UnitTests.Application.Category.Common;
+using Entities = LightDiet.Recipe.Domain.Entity;
+
+namespace LightDiet.Recipe.UnitTests.Application.Category.ListCategories;
+
+public class ListCategoriesTestFixture
+    : CategoryUseCasesBaseFixture
+{
+    public List<Entities.Category> GetListOfCategories(int quantityOfItens)
+    {
+        var list = new List<Entities.Category>();
+
+        for (int i = 0; i < quantityOfItens; i++)
+        {
+            list.Add(GetValidCategory());
+        }
+
+        return list;
+    }
+
+    public ListCategoriesInput GetExampleInput()
+    {
+        var random = new Random();
+
+        return new ListCategoriesInput(
+            page: random.Next(1, 10),
+            perPage: random.Next(15, 100),
+            search: Faker.Commerce.ProductName(),
+            sort: Faker.Commerce.ProductName(),
+            dir: random.Next(0, 10) > 5 ? SearchOrder.Asc : SearchOrder.Desc
+        );
+    }
+}
+
+[CollectionDefinition(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTestFixtureCollecion
+    : ICollectionFixture<ListCategoriesTestFixture>
+{
+
+}
